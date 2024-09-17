fb6ec2be19eda7866f4cccec532969ee53d7cb41
nesssch <v.schattman@gmail.com>
2024-09-17 13:18:13 -0400

    removes additional tests

--- python/translator.py
+++ python/translator.py
@@ -1,6 +1,6 @@
 import sys 
 
-ENG_TO_BRAILLE = { #converts English characters to Braille
+ENG_TO_BRAILLE = { #maps English characters to Braille
     "a": "O.....",
     "b": "O.O...",
     "c": "OO....",
@@ -51,26 +51,27 @@ ENG_TO_BRAILLE = { #converts English characters to Braille
     ")": ".O.OO.",
     " ": "......"
 }
-BRAILLE_TO_ENG_LET = {} #converts Braille characters to English numbers
-BRAILLE_TO_ENG_NUM = {} #converts Braille characters to English lowercase letters, including space
+BRAILLE_TO_ENG_LET = {} #maps Braille characters to English numbers
+BRAILLE_TO_ENG_NUM = {} #maps Braille characters to English lowercase letters; includes space character
 
-for key, value in ENG_TO_BRAILLE.items(): #Fills Braille dictionaries
+for key, value in ENG_TO_BRAILLE.items(): #Fills Braille-to-English dictionaries
     if key.isalpha() or key == " ":
         BRAILLE_TO_ENG_LET[value] = key
     else:
         BRAILLE_TO_ENG_NUM[value] = key
 
 def is_braille(text):
-    """Returns True if the text is braille, False otherwise"""
+    """Returns True if the input text is braille, False otherwise"""
     for char in text:
         if char != "O" and char != ".":
             return False
     return True
 
 def convert_e_to_b(text):
-    """Converts input text in English to its corresponding Braille text"""
-    braille = ""
-    wasnum = False
+    """Converts input text in English to its corresponding Braille text 
+    and returns the Braille"""
+    braille = "" #Braille output
+    wasnum = False #True when the current char is a number and the previous char wasn't, i.e. the char is the first number
     for char in text:
         if char.isupper():
             braille = braille + ".....O"
@@ -86,40 +87,38 @@ def convert_e_to_b(text):
     return braille
 
 def b_lookup (b_char, is_cap, is_num):
-    """great documentation"""
+    """Returns the correct English translation of the inputted braille character b_char
+    according to whether it is capitalised, numeric, or neither."""
     if is_num:
         return BRAILLE_TO_ENG_NUM[b_char]
    
     e_char = BRAILLE_TO_ENG_LET[b_char]
     if is_cap:
-        #print("Capital " + e_char.upper())
         return e_char.upper()
     return e_char
 
 def convert_b_to_e(text):
-    """Converts input text in Braille to its corresponding English text"""
-    english = ""
-    is_cap = False
-    is_num = False
+    """Converts input text in Braille to its corresponding English text
+    and returns the English text"""
+    english = "" #English output
+    is_cap = False #True if the next character is capitalised
+    is_num = False #True if the next character is a number
     for i in range(0, len(text), 6):
-        b_char = text[i:i+6]
-        #print("b_char = " + b_char)
+        b_char = text[i:i+6] #current Braille character
         if b_char == ".....O":
-            #print("Capital, just capital!")
             is_cap = True
         elif b_char == ".O.OOO":
             is_num = True
         else:
             if b_char == "......":
                 is_num = False
-            e_char = b_lookup(b_char, is_cap, is_num)
+            e_char = b_lookup(b_char, is_cap, is_num) #correct English char
             english = english + e_char
-            
             is_cap = False
-
     return english
 
 def main():
+    """Reads console input and returns its translation"""
     text = " ".join(sys.argv[1:])
     if is_braille(text):
         return convert_b_to_e(text)
